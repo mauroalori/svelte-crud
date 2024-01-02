aa2014d7ae580403100366701dcad045274ff799
Mauro Alori <mauroalori@hotmail.com>
2024-01-02 19:11:40 -0300

    agregar el puerto como variable de entorno

--- src/backend/config.js
+++ src/backend/config.js
@@ -3,3 +3,4 @@ import { config } from 'dotenv';
 config()
 
 export const DATABASE_URL = process.env['DATABASE_URL']
+export const PORT = process.env['PORT']
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,11 @@
 import connectToDB from './utils/postgres.js'
 import app from './app.js'
 
+import { PORT } from './config.js';
+
 // Cuando se ejecuta la IIFE, se realiza la conexion a la base de datos y la app empieza a correr en el puerto 3000
 (async function () {
     await connectToDB()
-    app.listen(3000)
+    app.listen(PORT)
     console.log('Server en puerto 3000 OK')
 }());
